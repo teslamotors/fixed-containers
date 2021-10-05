60833b5b2ca935ab542ff4a5cd8b01ed4453db44
Alexander Karatarakis <alex@karatarakis.com>
2021-10-04 18:53:56 -0700

    Introduce consteval compare

---
+++ include/fixed_containers/consteval_compare.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace fixed_containers::consteval_compare
+{
+// These will show the values if it fails to compile
+template <auto LHS, auto RHS>
+inline constexpr bool equal = LHS == RHS;
+template <auto LHS, auto RHS>
+inline constexpr bool not_equal = LHS != RHS;
+
+template <auto LHS, auto RHS>
+inline constexpr bool less_than = LHS < RHS;
+template <auto LHS, auto RHS>
+inline constexpr bool less_or_equal = LHS <= RHS;
+
+template <auto LHS, auto RHS>
+inline constexpr bool greater_than = LHS > RHS;
+template <auto LHS, auto RHS>
+inline constexpr bool greater_or_equal = LHS >= RHS;
+}  // namespace fixed_containers::consteval_compare
