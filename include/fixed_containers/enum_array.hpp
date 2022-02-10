ec09da86094664b583b04915fe45047d7b1395af
Alexander Karatarakis <alex@karatarakis.com>
2022-02-10 12:53:58 -0800

    Introduce EnumArray

---
+++ include/fixed_containers/enum_array.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "fixed_containers/enum_utils.hpp"
+
+#include <array>
+
+namespace fixed_containers::enum_array_detail
+{
+template <class L, class T>
+class EnumArray
+{
+    using EnumAdapterType = rich_enums::EnumAdapter<L>;
+    static constexpr std::size_t ENUM_COUNT = EnumAdapterType::count();
+    using LabelArrayType = std::array<L, ENUM_COUNT>;
+    using ValueArrayType = std::array<T, ENUM_COUNT>;
+    static constexpr const LabelArrayType& ENUM_VALUES = EnumAdapterType::values();
+
+public:
+    using value_type = typename ValueArrayType::value_type;
+    using size_type = typename ValueArrayType::size_type;
+    using difference_type = typename ValueArrayType::difference_type;
+    using reference = typename ValueArrayType::reference;
+    using const_reference = typename ValueArrayType::const_reference;
+    using pointer = typename ValueArrayType::pointer;
+    using const_pointer = typename ValueArrayType::const_pointer;
+    using iterator = typename ValueArrayType::iterator;
+    using const_iterator = typename ValueArrayType::const_iterator;
+    using reverse_iterator = typename ValueArrayType::reverse_iterator;
+    using const_reverse_iterator = typename ValueArrayType::const_reverse_iterator;
+
+private:
+    ValueArrayType values_;
+
+public:
+    constexpr EnumArray() noexcept
+      : values_()
+    {
+    }
+
+    constexpr EnumArray(std::initializer_list<std::pair<const L, T>> list) noexcept
+      : EnumArray()
+    {
+        for (const auto& [label, value] : list)
+        {
+            const std::size_t ordinal = EnumAdapterType::ordinal(label);
+            values_.at(ordinal) = value;
+        }
+    }
+
+public:
+    constexpr reference at(const L& label)
+    {
+        const std::size_t ordinal = EnumAdapterType::ordinal(label);
+        return values_.at(ordinal);
+    }
+    constexpr const_reference at(const L& label) const
+    {
+        const std::size_t ordinal = EnumAdapterType::ordinal(label);
+        return values_.at(ordinal);
+    }
+    constexpr reference operator[](const L& label)
+    {
+        const std::size_t ordinal = EnumAdapterType::ordinal(label);
+        return values_.at(ordinal);
+    }
+    constexpr const_reference operator[](const L& label) const
+    {
+        const std::size_t ordinal = EnumAdapterType::ordinal(label);
+        return values_.at(ordinal);
+    }
+    constexpr reference front() { return values_.front(); }
+    constexpr const_reference front() const { return values_.front(); }
+    constexpr reference back() { return values_.back(); }
+    constexpr const_reference back() const { return values_.back(); }
+
+    constexpr T* data() noexcept { return values_.data(); }
+    constexpr const T* data() const noexcept { return values_.data(); }
+
+    constexpr iterator begin() noexcept { return values_.begin(); }
+    constexpr const_iterator begin() const noexcept { return values_.begin(); }
+    constexpr const_iterator cbegin() const noexcept { return values_.cbegin(); }
+    constexpr iterator end() noexcept { return values_.end(); }
+    constexpr const_iterator end() const noexcept { return values_.end(); }
+    constexpr const_iterator cend() const noexcept { return values_.cend(); }
+
+    constexpr reverse_iterator rbegin() noexcept { return values_.rbegin(); }
+    constexpr const_reverse_iterator rbegin() const noexcept { return values_.rbegin(); }
+    constexpr const_reverse_iterator crbegin() const noexcept { return values_.crbegin(); }
+    constexpr reverse_iterator rend() noexcept { return values_.rend(); }
+    constexpr const_reverse_iterator rend() const noexcept { return values_.rend(); }
+    constexpr const_reverse_iterator crend() const noexcept { return values_.crend(); }
+
+    [[nodiscard]] constexpr bool empty() const noexcept { return values_.empty(); }
+    constexpr size_type size() const noexcept { return values_.size(); }
+    constexpr size_type max_size() const noexcept { return values_.max_size(); }
+
+    constexpr const LabelArrayType& labels() const noexcept { return ENUM_VALUES; }
+
+    constexpr void fill(const T& value) { values_.fill(value); }
+
+    constexpr void swap(EnumArray<L, T>& other) noexcept { return values_.swap(other.values_); }
+
+    constexpr bool operator==(const EnumArray<L, T>& other) const
+    {
+        return values_ == other.values_;
+    }
+    constexpr auto operator<=>(const EnumArray<L, T>& other) const
+    {
+        return values_ <=> other.values_;
+    }
+};
+}  // namespace fixed_containers::enum_array_detail
---
+++ test/enum_array_test.cpp
@@ -0,0 +1,335 @@
+#include "fixed_containers/enum_array.hpp"
+
+#include "enums_test_common.hpp"
+
+#include "fixed_containers/consteval_compare.hpp"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+namespace fixed_containers::enum_array_detail
+{
+namespace
+{
+using TestEnum1 = rich_enums::TestEnum1;
+using EnumWithNoConstants = rich_enums::EnumWithNoConstants;
+
+static_assert(std::is_trivially_copyable_v<EnumArray<TestEnum1, int>>);
+}  // namespace
+
+TEST(EnumArray, DefaultConstructorDefaultInitialization)
+{
+    {
+        constexpr EnumArray<TestEnum1, int> s1{};
+        static_assert(consteval_compare::equal<4, s1.max_size()>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::ONE)>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::TWO)>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::THREE)>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::FOUR)>);
+    }
+
+    {
+        EnumArray<TestEnum1, std::unique_ptr<int>> s2{};
+        EXPECT_EQ(4, s2.max_size());
+        EXPECT_EQ(nullptr, s2.at(TestEnum1::ONE));
+    }
+}
+
+TEST(EnumArray, DefaultConstructorValueInitialization)
+{
+    {
+        constexpr EnumArray<TestEnum1, int> s1{};
+        static_assert(consteval_compare::equal<4, s1.max_size()>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::ONE)>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::TWO)>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::THREE)>);
+        static_assert(consteval_compare::equal<0, s1.at(TestEnum1::FOUR)>);
+    }
+
+    {
+        EnumArray<TestEnum1, std::unique_ptr<int>> s2{};
+        EXPECT_EQ(4, s2.max_size());
+        EXPECT_EQ(nullptr, s2.at(TestEnum1::ONE));
+    }
+}
+
+TEST(EnumArray, AggregateInitialization)
+{
+    constexpr EnumArray<TestEnum1, int> s1{
+        {TestEnum1::ONE, 10}, {TestEnum1::TWO, 20}, {TestEnum1::THREE, 30}, {TestEnum1::FOUR, 40}};
+    static_assert(consteval_compare::equal<4, s1.max_size()>);
+    static_assert(consteval_compare::equal<10, s1.at(TestEnum1::ONE)>);
+    static_assert(consteval_compare::equal<20, s1.at(TestEnum1::TWO)>);
+    static_assert(consteval_compare::equal<30, s1.at(TestEnum1::THREE)>);
+    static_assert(consteval_compare::equal<40, s1.at(TestEnum1::FOUR)>);
+}
+
+TEST(EnumArray, At)
+{
+    {
+        constexpr EnumArray<TestEnum1, int> s1{{TestEnum1::ONE, 10},
+                                               {TestEnum1::TWO, 20},
+                                               {TestEnum1::THREE, 30},
+                                               {TestEnum1::FOUR, 40}};
+        static_assert(consteval_compare::equal<4, s1.max_size()>);
+        static_assert(consteval_compare::equal<10, s1.at(TestEnum1::ONE)>);
+        static_assert(consteval_compare::equal<20, s1.at(TestEnum1::TWO)>);
+        static_assert(consteval_compare::equal<30, s1.at(TestEnum1::THREE)>);
+        static_assert(consteval_compare::equal<40, s1.at(TestEnum1::FOUR)>);
+    }
+
+    {
+        EnumArray<TestEnum1, std::unique_ptr<int>> s2{};
+        s2.at(TestEnum1::ONE) = std::make_unique<int>(5);
+        EXPECT_EQ(5, *s2.at(TestEnum1::ONE));
+    }
+}
+
+TEST(EnumArray, OperatorBracket)
+{
+    constexpr EnumArray<TestEnum1, int> s1{
+        {TestEnum1::ONE, 10}, {TestEnum1::TWO, 20}, {TestEnum1::THREE, 30}, {TestEnum1::FOUR, 40}};
+    static_assert(consteval_compare::equal<4, s1.max_size()>);
+    static_assert(consteval_compare::equal<10, s1[TestEnum1::ONE]>);
+    static_assert(consteval_compare::equal<20, s1[TestEnum1::TWO]>);
+    static_assert(consteval_compare::equal<30, s1[TestEnum1::THREE]>);
+    static_assert(consteval_compare::equal<40, s1[TestEnum1::FOUR]>);
+}
+
+TEST(EnumArray, Front)
+{
+    constexpr EnumArray<TestEnum1, int> s1{
+        {TestEnum1::ONE, 10}, {TestEnum1::TWO, 20}, {TestEnum1::THREE, 30}, {TestEnum1::FOUR, 40}};
+    static_assert(consteval_compare::equal<10, s1.front()>);
+}
+
+TEST(EnumArray, Back)
+{
+    constexpr EnumArray<TestEnum1, int> s1{
+        {TestEnum1::ONE, 10}, {TestEnum1::TWO, 20}, {TestEnum1::THREE, 30}, {TestEnum1::FOUR, 40}};
+    static_assert(consteval_compare::equal<40, s1.back()>);
+}
+
+TEST(EnumArray, Data)
+{
+    constexpr EnumArray<TestEnum1, int> s1{
+        {TestEnum1::ONE, 10}, {TestEnum1::TWO, 20}, {TestEnum1::THREE, 30}, {TestEnum1::FOUR, 40}};
+    static_assert(consteval_compare::equal<10, s1.data()[0]>);
+    static_assert(consteval_compare::equal<40, s1.data()[3]>);
+}
+
+TEST(EnumArray, IteratorUsage)
+{
+    // Traditional std::array
+    {
+        // Untyped access
+        std::array<int, magic_enum::enum_count<TestEnum1>()> s1{};
+        for (std::size_t i = 0; i < s1.max_size(); i++)
+        {
+            s1[i] += 1;
+        }
+
+        // Typed access
+        for (const TestEnum1& label : magic_enum::enum_values<TestEnum1>())
+        {
+            s1[magic_enum::enum_index(label).value()] += 1;
+        }
+
+        // Typed access that relies on the enum constant values!
+        for (const TestEnum1& label : magic_enum::enum_values<TestEnum1>())
+        {
+            s1[static_cast<std::size_t>(label)] += 1;
+        }
+    }
+
+    // EnumArray
+    {
+        EnumArray<TestEnum1, int> s1{};
+        for (const TestEnum1& label : s1.labels())
+        {
+            s1[label] += 1;
+        }
+    }
+}
+
+TEST(EnumArray, IteratorBasic)
+{
+    {
+        constexpr EnumArray<TestEnum1, int> s1 = []()
+        {
+            EnumArray<TestEnum1, int> s{{TestEnum1::ONE, 10},
+                                        {TestEnum1::TWO, 20},
+                                        {TestEnum1::THREE, 30},
+                                        {TestEnum1::FOUR, 40}};
+
+            for (const TestEnum1& label : s.labels())
+            {
+                s[label] += 1;
+            }
+
+            return s;
+        }();
+
+        static_assert(consteval_compare::equal<4, s1.max_size()>);
+        static_assert(consteval_compare::equal<11, s1[TestEnum1::ONE]>);
+        static_assert(consteval_compare::equal<21, s1[TestEnum1::TWO]>);
+        static_assert(consteval_compare::equal<31, s1[TestEnum1::THREE]>);
+        static_assert(consteval_compare::equal<41, s1[TestEnum1::FOUR]>);
+    }
+
+    {
+        constexpr EnumArray<TestEnum1, int> s1{{TestEnum1::ONE, 10},
+                                               {TestEnum1::TWO, 20},
+                                               {TestEnum1::THREE, 30},
+                                               {TestEnum1::FOUR, 40}};
+
+        static_assert(std::distance(s1.cbegin(), s1.cend()) == 4);
+
+        static_assert(*s1.begin() == 10);
+        static_assert(*std::next(s1.begin(), 1) == 20);
+        static_assert(*std::next(s1.begin(), 2) == 30);
+        static_assert(*std::next(s1.begin(), 3) == 40);
+
+        static_assert(*std::prev(s1.end(), 1) == 40);
+        static_assert(*std::prev(s1.end(), 2) == 30);
+        static_assert(*std::prev(s1.end(), 3) == 20);
+        static_assert(*std::prev(s1.end(), 4) == 10);
+    }
+}
+
+TEST(EnumArray, ReverseIteratorBasic)
+{
+    constexpr EnumArray<TestEnum1, int> s1{
+        {TestEnum1::ONE, 10}, {TestEnum1::TWO, 20}, {TestEnum1::THREE, 30}, {TestEnum1::FOUR, 40}};
+
+    static_assert(std::distance(s1.crbegin(), s1.crend()) == 4);
+
+    static_assert(*s1.rbegin() == 40);
+    static_assert(*std::next(s1.rbegin(), 1) == 30);
+    static_assert(*std::next(s1.crbegin(), 2) == 20);
+    static_assert(*std::next(s1.rbegin(), 3) == 10);
+
+    static_assert(*std::prev(s1.rend(), 1) == 10);
+    static_assert(*std::prev(s1.crend(), 2) == 20);
+    static_assert(*std::prev(s1.rend(), 3) == 30);
+    static_assert(*std::prev(s1.rend(), 4) == 40);
+}
+
+TEST(EnumArray, Empty)
+{
+    constexpr EnumArray<TestEnum1, int> s1{};
+    static_assert(!s1.empty());
+
+    constexpr EnumArray<EnumWithNoConstants, int> s2{};
+    static_assert(s2.empty());
+}
+
+TEST(EnumArray, Size)
+{
+    constexpr EnumArray<TestEnum1, int> s1{};
+    static_assert(consteval_compare::equal<4, s1.size()>);
+
+    constexpr EnumArray<EnumWithNoConstants, int> s2{};
+    static_assert(consteval_compare::equal<0, s2.size()>);
+}
+
+TEST(EnumArray, MaxSize)
+{
+    constexpr EnumArray<TestEnum1, int> s1{};
+    static_assert(consteval_compare::equal<4, s1.max_size()>);
+
+    constexpr EnumArray<EnumWithNoConstants, int> s2{};
+    static_assert(consteval_compare::equal<0, s2.max_size()>);
+}
+
+TEST(EnumArray, Labels)
+{
+    constexpr EnumArray<TestEnum1, int> s1{};
+    static_assert(consteval_compare::equal<4, s1.labels().max_size()>);
+}
+
+TEST(EnumArray, Fill)
+{
+    constexpr EnumArray<TestEnum1, int> s1 = []()
+    {
+        EnumArray<TestEnum1, int> s{};
+        s.fill(5);
+        return s;
+    }();
+
+    static_assert(consteval_compare::equal<4, s1.max_size()>);
+    static_assert(consteval_compare::equal<5, s1[TestEnum1::ONE]>);
+    static_assert(consteval_compare::equal<5, s1[TestEnum1::TWO]>);
+    static_assert(consteval_compare::equal<5, s1[TestEnum1::THREE]>);
+    static_assert(consteval_compare::equal<5, s1[TestEnum1::FOUR]>);
+}
+
+TEST(EnumArray, Swap)
+{
+    constexpr EnumArray<TestEnum1, int> s1 = []()
+    {
+        EnumArray<TestEnum1, int> s{{TestEnum1::ONE, 10},
+                                    {TestEnum1::TWO, 20},
+                                    {TestEnum1::THREE, 30},
+                                    {TestEnum1::FOUR, 40}};
+
+        EnumArray<TestEnum1, int> s_other{{TestEnum1::ONE, 101},
+                                          {TestEnum1::TWO, 201},
+                                          {TestEnum1::THREE, 301},
+                                          {TestEnum1::FOUR, 401}};
+
+        s.swap(s_other);
+        return s;
+    }();
+
+    static_assert(consteval_compare::equal<4, s1.max_size()>);
+    static_assert(consteval_compare::equal<101, s1[TestEnum1::ONE]>);
+    static_assert(consteval_compare::equal<201, s1[TestEnum1::TWO]>);
+    static_assert(consteval_compare::equal<301, s1[TestEnum1::THREE]>);
+    static_assert(consteval_compare::equal<401, s1[TestEnum1::FOUR]>);
+}
+
+TEST(EnumArray, Equality)
+{
+    {
+        constexpr EnumArray<TestEnum1, int> s1{{TestEnum1::ONE, 10}, {TestEnum1::FOUR, 40}};
+        constexpr EnumArray<TestEnum1, int> s2{{TestEnum1::FOUR, 40}, {TestEnum1::ONE, 10}};
+        constexpr EnumArray<TestEnum1, int> s3{{TestEnum1::ONE, 10}, {TestEnum1::THREE, 30}};
+        constexpr EnumArray<TestEnum1, int> s4{{TestEnum1::ONE, 10}};
+
+        static_assert(s1 == s2);
+        static_assert(s2 == s1);
+
+        static_assert(s1 != s3);
+        static_assert(s3 != s1);
+
+        static_assert(s1 != s4);
+        static_assert(s4 != s1);
+    }
+
+    // Values
+    {
+        constexpr EnumArray<TestEnum1, int> s1{{TestEnum1::ONE, 10}, {TestEnum1::FOUR, 40}};
+        constexpr EnumArray<TestEnum1, int> s2{{TestEnum1::ONE, 10}, {TestEnum1::FOUR, 44}};
+        constexpr EnumArray<TestEnum1, int> s3{{TestEnum1::ONE, 40}, {TestEnum1::FOUR, 10}};
+
+        static_assert(s1 != s2);
+        static_assert(s1 != s3);
+    }
+}
+
+TEST(EnumArray, Comparison)
+{
+    {
+        constexpr EnumArray<TestEnum1, int> s1{{TestEnum1::ONE, 10}, {TestEnum1::FOUR, 40}};
+        constexpr EnumArray<TestEnum1, int> s2{{TestEnum1::ONE, 11}, {TestEnum1::FOUR, 400000}};
+
+        static_assert(s1 < s2);
+        static_assert(s1 <= s2);
+        static_assert(s2 > s1);
+        static_assert(s2 >= s1);
+    }
+}
+
+}  // namespace fixed_containers::enum_array_detail
