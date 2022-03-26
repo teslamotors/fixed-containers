607ea4716b6717e1c8a7f5d554f6124f9ed66dc1
Alexander Karatarakis <alex@karatarakis.com>
2022-03-26 15:43:40 -0700

    [Optional/IndexOrValueStorage] Use concepts to split trivially_copyable vs not
    It is a little bit less code, but mostly in preparation for
    adding support for references

--- include/fixed_containers/index_or_value_storage.hpp
+++ include/fixed_containers/index_or_value_storage.hpp
@@ -7,31 +7,6 @@
 
 namespace fixed_containers::index_or_value_storage_detail
 {
-namespace index_or_value_storage_trivially_copyable
-{
-template <class T>
-union IndexOrValueStorage
-{
-    std::size_t index;
-    T value;
-    // clang-format off
-    constexpr IndexOrValueStorage() noexcept : index{} { }
-    constexpr IndexOrValueStorage(const T& v) : value{v} { }
-    constexpr IndexOrValueStorage(T&& v) : value{std::move(v)} { }
-    template <class... Args>
-    constexpr IndexOrValueStorage(Args&&... args) : value(std::forward<Args>(args)...) { }
-    // clang-format on
-    constexpr IndexOrValueStorage(const IndexOrValueStorage&) = default;
-    constexpr IndexOrValueStorage(IndexOrValueStorage&&) noexcept = default;
-    constexpr IndexOrValueStorage& operator=(const IndexOrValueStorage&) = default;
-    constexpr IndexOrValueStorage& operator=(IndexOrValueStorage&&) noexcept = default;
-    constexpr ~IndexOrValueStorage() noexcept = default;
-};
-
-}  // namespace index_or_value_storage_trivially_copyable
-
-namespace index_or_value_storage_non_trivially_copyable
-{
 template <class T>
 union IndexOrValueStorage
 {
@@ -68,20 +43,30 @@ union IndexOrValueStorage
     constexpr ~IndexOrValueStorage() noexcept {}
 };
 
-}  // namespace index_or_value_storage_non_trivially_copyable
-
 // IndexOrValueStorage<T> should carry the properties of T. For example, if T fulfils
 // std::is_trivially_copy_assignable<T>, then so should IndexOrValueStorage<T>.
 // This is done with concepts. However, at the time of writing there is a compiler bug
 // that is preventing usage of concepts for destructors: https://bugs.llvm.org/show_bug.cgi?id=46269
-// WORKAROUND due to destructors: manually do the split with std::conditional_t.
+// WORKAROUND due to destructors: manually do the split with template specialization.
 // NOTE: we branch on TriviallyCopyable instead of TriviallyDestructible because it needs all
 // special functions to be trivial. The NonTriviallyCopyable flavor handles triviality separately
 // for each special function (except the destructor).
-template <class T>
-using IndexOrValueStorage =
-    std::conditional_t<TriviallyCopyable<T>,
-                       index_or_value_storage_trivially_copyable::IndexOrValueStorage<T>,
-                       index_or_value_storage_non_trivially_copyable::IndexOrValueStorage<T>>;
-
+template <TriviallyCopyable T>
+union IndexOrValueStorage<T>
+{
+    std::size_t index;
+    T value;
+    // clang-format off
+    constexpr IndexOrValueStorage() noexcept : index{} { }
+    constexpr IndexOrValueStorage(const T& v) : value{v} { }
+    constexpr IndexOrValueStorage(T&& v) : value{std::move(v)} { }
+    template <class... Args>
+    constexpr IndexOrValueStorage(Args&&... args) : value(std::forward<Args>(args)...) { }
+    // clang-format on
+    constexpr IndexOrValueStorage(const IndexOrValueStorage&) = default;
+    constexpr IndexOrValueStorage(IndexOrValueStorage&&) noexcept = default;
+    constexpr IndexOrValueStorage& operator=(const IndexOrValueStorage&) = default;
+    constexpr IndexOrValueStorage& operator=(IndexOrValueStorage&&) noexcept = default;
+    constexpr ~IndexOrValueStorage() noexcept = default;
+};
 }  // namespace fixed_containers::index_or_value_storage_detail
--- include/fixed_containers/optional_storage.hpp
+++ include/fixed_containers/optional_storage.hpp
@@ -11,29 +11,6 @@ struct OptionalStorageDummyT
 {
 };
 
-namespace optional_storage_trivially_copyable
-{
-template <class T>
-union OptionalStorage
-{
-    detail::OptionalStorageDummyT dummy;
-    T value;
-    // clang-format off
-    constexpr OptionalStorage() noexcept : dummy{} { }
-    constexpr OptionalStorage(const T& v) : value{v} { }
-    constexpr OptionalStorage(T&& v) : value{std::move(v)} { }
-    // clang-format on
-    constexpr OptionalStorage(const OptionalStorage&) = default;
-    constexpr OptionalStorage(OptionalStorage&&) noexcept = default;
-    constexpr OptionalStorage& operator=(const OptionalStorage&) = default;
-    constexpr OptionalStorage& operator=(OptionalStorage&&) noexcept = default;
-    constexpr ~OptionalStorage() noexcept = default;
-};
-
-}  // namespace optional_storage_trivially_copyable
-
-namespace optional_storage_non_trivially_copyable
-{
 template <class T>
 union OptionalStorage
 {
@@ -68,20 +45,29 @@ union OptionalStorage
     constexpr ~OptionalStorage() noexcept {}
 };
 
-}  // namespace optional_storage_non_trivially_copyable
-
 // OptionalStorage<T> should carry the properties of T. For example, if T fulfils
 // std::is_trivially_copy_assignable<T>, then so should OptionalStorage<T>.
 // This is done with concepts. However, at the time of writing there is a compiler bug
 // that is preventing usage of concepts for destructors: https://bugs.llvm.org/show_bug.cgi?id=46269
-// WORKAROUND due to destructors: manually do the split with std::conditional_t.
+// WORKAROUND due to destructors: manually do the split with template specialization.
 // NOTE: we branch on TriviallyCopyable instead of TriviallyDestructible because it needs all
 // special functions to be trivial. The NonTriviallyCopyable flavor handles triviality separately
 // for each special function (except the destructor).
-template <class T>
-using OptionalStorage =
-    std::conditional_t<TriviallyCopyable<T>,
-                       optional_storage_trivially_copyable::OptionalStorage<T>,
-                       optional_storage_non_trivially_copyable::OptionalStorage<T>>;
 
+template <TriviallyCopyable T>
+union OptionalStorage<T>
+{
+    detail::OptionalStorageDummyT dummy;
+    T value;
+    // clang-format off
+    constexpr OptionalStorage() noexcept : dummy{} { }
+    constexpr OptionalStorage(const T& v) : value{v} { }
+    constexpr OptionalStorage(T&& v) : value{std::move(v)} { }
+    // clang-format on
+    constexpr OptionalStorage(const OptionalStorage&) = default;
+    constexpr OptionalStorage(OptionalStorage&&) noexcept = default;
+    constexpr OptionalStorage& operator=(const OptionalStorage&) = default;
+    constexpr OptionalStorage& operator=(OptionalStorage&&) noexcept = default;
+    constexpr ~OptionalStorage() noexcept = default;
+};
 }  // namespace fixed_containers::detail
