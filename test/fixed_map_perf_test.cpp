46583dadd1d1325ea076c6b1831fd117afcd170c
Alexander Karatarakis <alex@karatarakis.com>
2022-03-26 19:40:52 -0700

    Add tests for all FixedMap flavors
    This is also indirectly ensures that all flavors compile

--- test/fixed_map_perf_test.cpp
+++ test/fixed_map_perf_test.cpp
@@ -9,6 +9,16 @@ using V = std::array<std::array<int, 3>, 30>;
 static constexpr std::size_t CAP = 130;
 
 template <class K, class V, std::size_t MAXIMUM_SIZE>
+using CompactPoolFixedMap =
+    FixedMap<K,
+             V,
+             MAXIMUM_SIZE,
+             std::less<int>,
+             fixed_red_black_tree_detail::RedBlackTreeNodeColorCompactness::EMBEDDED_COLOR(),
+             FixedIndexBasedPoolStorage>;
+static_assert(std::is_same_v<FixedMap<int, V, CAP>, CompactPoolFixedMap<int, V, CAP>>);
+
+template <class K, class V, std::size_t MAXIMUM_SIZE>
 using CompactContiguousFixedMap =
     FixedMap<K,
              V,
@@ -17,10 +27,30 @@ using CompactContiguousFixedMap =
              fixed_red_black_tree_detail::RedBlackTreeNodeColorCompactness::EMBEDDED_COLOR(),
              FixedIndexBasedContiguousStorage>;
 
-using A = FixedMap<int, V, CAP>;
-using B = CompactContiguousFixedMap<int, V, CAP>;
+template <class K, class V, std::size_t MAXIMUM_SIZE>
+using DedicatedColorBitPoolFixedMap =
+    FixedMap<K,
+             V,
+             MAXIMUM_SIZE,
+             std::less<int>,
+             fixed_red_black_tree_detail::RedBlackTreeNodeColorCompactness::DEDICATED_COLOR(),
+             FixedIndexBasedContiguousStorage>;
+
+template <class K, class V, std::size_t MAXIMUM_SIZE>
+using DedicatedColorBitContiguousFixedMap =
+    FixedMap<K,
+             V,
+             MAXIMUM_SIZE,
+             std::less<int>,
+             fixed_red_black_tree_detail::RedBlackTreeNodeColorCompactness::DEDICATED_COLOR(),
+             FixedIndexBasedContiguousStorage>;
+
 // The reference boost-based fixed_map (with an array-backed pool-allocator) was at 51000
 // at the time of writing.
-static_assert(consteval_compare::equal<50992, sizeof(A)>);
-static_assert(consteval_compare::equal<50992, sizeof(B)>);
+static_assert(consteval_compare::equal<50992, sizeof(FixedMap<int, V, CAP>)>);
+static_assert(consteval_compare::equal<50992, sizeof(CompactPoolFixedMap<int, V, CAP>)>);
+static_assert(consteval_compare::equal<50992, sizeof(CompactContiguousFixedMap<int, V, CAP>)>);
+static_assert(consteval_compare::equal<52032, sizeof(DedicatedColorBitPoolFixedMap<int, V, CAP>)>);
+static_assert(
+    consteval_compare::equal<52032, sizeof(DedicatedColorBitContiguousFixedMap<int, V, CAP>)>);
 }  // namespace fixed_containers
