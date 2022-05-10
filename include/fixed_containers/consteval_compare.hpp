ccdb6ee6e584643fc5553d66892056837d18ab40
Alexander Karatarakis <alex@karatarakis.com>
2022-05-10 15:35:07 -0700

    [consteval_cmpare] less_than -> less, greater_than -> greater

--- include/fixed_containers/consteval_compare.hpp
+++ include/fixed_containers/consteval_compare.hpp
@@ -9,12 +9,12 @@ template <auto LHS, auto RHS>
 inline constexpr bool not_equal = LHS != RHS;
 
 template <auto LHS, auto RHS>
-inline constexpr bool less_than = LHS < RHS;
+inline constexpr bool less = LHS < RHS;
 template <auto LHS, auto RHS>
 inline constexpr bool less_or_equal = LHS <= RHS;
 
 template <auto LHS, auto RHS>
-inline constexpr bool greater_than = LHS > RHS;
+inline constexpr bool greater = LHS > RHS;
 template <auto LHS, auto RHS>
 inline constexpr bool greater_or_equal = LHS >= RHS;
 }  // namespace fixed_containers::consteval_compare
