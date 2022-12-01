da4a0e0407de8cc5e2b53ccfdd57299bdf5d7f50
Alexander Karatarakis <alex@karatarakis.com>
2022-12-01 15:10:57 -0800

    Add storage for FixedDeque and size()/max_size()/empty()

--- include/fixed_containers/fixed_deque.hpp
+++ include/fixed_containers/fixed_deque.hpp
@@ -1,12 +1,33 @@
 #pragma once
 
+#include "fixed_containers/optional_storage.hpp"
+
+#include <array>
 #include <cstddef>
 
 namespace fixed_containers
 {
-template <typename /*T*/, std::size_t /*MAXIMUM_SIZE*/>
+template <typename T, std::size_t MAXIMUM_SIZE>
 class FixedDeque
 {
+    using OptionalT = optional_storage_detail::OptionalStorage<T>;
+
+private:
+    std::array<OptionalT, MAXIMUM_SIZE> array_;
+    std::size_t index_i_;
+    std::size_t index_j_;
+
+public:
+    constexpr FixedDeque() noexcept
+      : index_i_(0)
+      , index_j_(0)
+    // Don't initialize the array
+    {
+    }
+
+    [[nodiscard]] constexpr std::size_t max_size() const noexcept { return MAXIMUM_SIZE; }
+    [[nodiscard]] constexpr std::size_t size() const noexcept { return index_j_ - index_i_; }
+    [[nodiscard]] constexpr bool empty() const noexcept { return size() == 0; }
 };
 
 }  // namespace fixed_containers
--- test/fixed_deque_test.cpp
+++ test/fixed_deque_test.cpp
@@ -9,4 +9,26 @@ TEST(FixedDeque, DefaultConstructor)
     constexpr FixedDeque<int, 8> v1{};
     (void)v1;
 }
+
+TEST(FixedDeque, MaxSize)
+{
+    {
+        constexpr FixedDeque<int, 3> v1{};
+        static_assert(v1.max_size() == 3);
+    }
+
+    {
+        FixedDeque<int, 3> v1{};
+        EXPECT_EQ(3, v1.max_size());
+    }
+}
+
+TEST(FixedDeque, Empty)
+{
+    constexpr auto v1 = []() { return FixedDeque<int, 7>{}; }();
+
+    static_assert(v1.empty());
+    static_assert(v1.max_size() == 7);
+}
+
 }  // namespace fixed_containers
