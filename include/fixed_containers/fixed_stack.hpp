944168b5841595306bb0c40a4aa778a9ae52ea3f
Alexander Karatarakis <alex@karatarakis.com>
2023-08-04 14:11:35 -0700

    Implement FixedStack

--- include/fixed_containers/fixed_stack.hpp
+++ include/fixed_containers/fixed_stack.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "fixed_containers/fixed_vector.hpp"
+#include "fixed_containers/source_location.hpp"
 
 #include <cstddef>
 
@@ -9,7 +10,14 @@ namespace fixed_containers::fixed_stack_detail
 template <typename T, std::size_t MAXIMUM_SIZE>
 class FixedStack
 {
-    FixedVector<T, MAXIMUM_SIZE> data_{};
+public:
+    using container_type = FixedVector<T, MAXIMUM_SIZE>;
+    using value_type = typename container_type::value_type;
+    using size_type = typename container_type::size_type;
+    using reference = typename container_type::reference;
+    using const_reference = typename container_type::const_reference;
+
+    container_type data_{};
 
 public:
     constexpr FixedStack()
@@ -17,10 +25,55 @@ public:
     {
     }
 
+    template <InputIterator InputIt>
+    constexpr FixedStack(InputIt first,
+                         InputIt last,
+                         const std_transition::source_location& loc =
+                             std_transition::source_location::current()) noexcept
+      : data_(first, last, loc)
+    {
+    }
+
 public:
     [[nodiscard]] constexpr std::size_t max_size() const noexcept { return MAXIMUM_SIZE; }
     [[nodiscard]] constexpr std::size_t size() const noexcept { return data_.size(); }
     [[nodiscard]] constexpr bool empty() const noexcept { return size() == 0; }
+
+    constexpr reference top(
+        const std_transition::source_location& loc = std_transition::source_location::current())
+    {
+        return data_.back(loc);
+    }
+    constexpr const_reference top(const std_transition::source_location& loc =
+                                      std_transition::source_location::current()) const
+    {
+        return data_.back(loc);
+    }
+
+    constexpr void push(
+        const value_type& value,
+        const std_transition::source_location& loc = std_transition::source_location::current())
+    {
+        data_.push_back(value, loc);
+    }
+    constexpr void push(
+        value_type&& value,
+        const std_transition::source_location& loc = std_transition::source_location::current())
+    {
+        data_.push_back(std::move(value), loc);
+    }
+
+    template <class... Args>
+    constexpr decltype(auto) emplace(Args&&... args)
+    {
+        data_.emplace_back(std::forward<Args>(args)...);
+    }
+
+    constexpr void pop(
+        const std_transition::source_location& loc = std_transition::source_location::current())
+    {
+        data_.pop_back(loc);
+    }
 };
 
 }  // namespace fixed_containers::fixed_stack_detail
--- test/fixed_stack_test.cpp
+++ test/fixed_stack_test.cpp
@@ -27,6 +27,18 @@ TEST(FixedStack, DefaultConstructor)
     (void)v1;
 }
 
+TEST(FixedStack, IteratorConstructor)
+{
+    constexpr FixedStack<int, 3> s1 = []()
+    {
+        FixedVector<int, 3> v1{77, 99};
+        return FixedStack<int, 3>{v1.begin(), v1.end()};
+    }();
+
+    static_assert(s1.top() == 99);
+    static_assert(s1.size() == 2);
+}
+
 TEST(FixedStack, MaxSize)
 {
     {
@@ -46,6 +58,75 @@ TEST(FixedStack, Empty)
 
     static_assert(v1.empty());
     static_assert(v1.max_size() == 7);
+}
+
+TEST(FixedStack, Top)
+{
+    {
+        constexpr FixedStack<int, 3> s1 = []()
+        {
+            FixedVector<int, 3> v1{77, 99};
+            return FixedStack<int, 3>{v1.begin(), v1.end()};
+        }();
+
+        static_assert(s1.top() == 99);
+        static_assert(s1.size() == 2);
+    }
+
+    {
+        FixedStack<int, 3> s1 = []()
+        {
+            FixedVector<int, 3> v1{77, 99};
+            return FixedStack<int, 3>{v1.begin(), v1.end()};
+        }();
+
+        ASSERT_EQ(99, s1.top());
+        ASSERT_EQ(2, s1.size());
+    }
+}
+
+TEST(FixedStack, Push)
+{
+    constexpr FixedStack<int, 3> s1 = []()
+    {
+        FixedStack<int, 3> v1{};
+        int my_int = 77;
+        v1.push(my_int);
+        v1.push(99);
+        return v1;
+    }();
+
+    static_assert(s1.top() == 99);
+    static_assert(s1.size() == 2);
+}
+
+TEST(FixedStack, Emplace)
+{
+    constexpr FixedStack<int, 3> s1 = []()
+    {
+        FixedStack<int, 3> v1{};
+        int my_int = 77;
+        v1.emplace(my_int);
+        v1.emplace(99);
+        return v1;
+    }();
+
+    static_assert(s1.top() == 99);
+    static_assert(s1.size() == 2);
+}
+
+TEST(FixedStack, Pop)
+{
+    constexpr FixedStack<int, 3> s1 = []()
+    {
+        FixedVector<int, 3> v1{77, 99};
+        FixedStack<int, 3> out{v1.begin(), v1.end()};
+        out.pop();
+        return out;
+    }();
+
+    static_assert(s1.top() == 77);
+    static_assert(s1.size() == 1);
 }
 
 }  // namespace fixed_containers::fixed_stack_detail
