66009669464db1b7467b33324fe2a31f6a24bb61
Alexander Karatarakis <alex@karatarakis.com>
2023-09-21 00:47:55 -0700

    Introduce `emplace_move_backward()`

---
+++ include/fixed_containers/algorithm.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <memory>
+
+namespace fixed_containers::algorithm
+{
+// Similar to https://en.cppreference.com/w/cpp/algorithm/move_backward
+// but uses `std::construct_at()`
+// There seems to be an issue with clang and setting the active union member in a constexpr context.
+template <class BidirIt1, class BidirIt2>
+constexpr BidirIt2 emplace_move_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last)
+{
+    while (first != last)
+    {
+        --d_last;
+        --last;
+        std::construct_at(&*d_last, *last);
+        std::destroy_at(&*last);
+    }
+    return d_last;
+}
+}  // namespace fixed_containers::algorithm
