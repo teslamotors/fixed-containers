758452cff70b23b6cbf2be6b91cb6c00c501d49f
Alexander Karatarakis <alex@karatarakis.com>
2024-02-24 14:34:08 -0800

    wyhash fixes: includes, constexpr, diagnostics

--- include/fixed_containers/wyhash.hpp
+++ include/fixed_containers/wyhash.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <cstring>
 #include <functional>
+#include <iterator>
 #include <memory>
+#include <span>
+#include <string>
 
 // This is a stripped-down implementation of wyhash: https://github.com/wangyi-fudan/wyhash
 // No big-endian support (because different values on different machines don't matter),
@@ -49,14 +53,14 @@ constexpr inline void mum(std::uint64_t* a, std::uint64_t* b)
 }
 
 // read functions. WARNING: we don't care about endianness, so results are different on big endian!
-[[nodiscard]] constexpr inline auto r8(const std::uint8_t* p) -> std::uint64_t
+[[nodiscard]] inline auto r8(const std::uint8_t* p) -> std::uint64_t
 {
     std::uint64_t v{};
     std::memcpy(&v, p, 8U);
     return v;
 }
 
-[[nodiscard]] constexpr inline auto r4(const std::uint8_t* p) -> std::uint64_t
+[[nodiscard]] inline auto r4(const std::uint8_t* p) -> std::uint64_t
 {
     std::uint32_t v{};
     std::memcpy(&v, p, 4);
@@ -64,14 +68,13 @@ constexpr inline void mum(std::uint64_t* a, std::uint64_t* b)
 }
 
 // reads 1, 2, or 3 bytes
-[[nodiscard]] constexpr inline auto r3(const std::uint8_t* p, size_t k) -> std::uint64_t
+[[nodiscard]] inline auto r3(const std::uint8_t* p, std::int64_t k) -> std::uint64_t
 {
-    return (static_cast<std::uint64_t>(p[0]) << 16U) |
-           (static_cast<std::uint64_t>(p[k >> 1U]) << 8U) | p[k - 1];
+    return (static_cast<std::uint64_t>(*p) << 16U) |
+           (static_cast<std::uint64_t>(*std::next(p, k >> 1U)) << 8U) | *std::next(p, k - 1);
 }
 
-[[maybe_unused]] [[nodiscard]] constexpr inline auto hash(void const* key, size_t len)
-    -> std::uint64_t
+[[maybe_unused]] [[nodiscard]] inline auto hash(void const* key, std::int64_t len) -> std::uint64_t
 {
     constexpr auto secret = std::array{UINT64_C(0xa0761d6478bd642f),
                                        UINT64_C(0xe7037ed1a0b428db),
@@ -86,8 +89,9 @@ constexpr inline void mum(std::uint64_t* a, std::uint64_t* b)
     {
         if (len >= 4)
         {
-            a = (r4(p) << 32U) | r4(p + ((len >> 3U) << 2U));
-            b = (r4(p + len - 4) << 32U) | r4(p + len - 4 - ((len >> 3U) << 2U));
+            a = (r4(p) << 32U) | r4(std::next(p, (len >> 3U) << 2U));
+            b = (r4(std::next(p, len - 4)) << 32U) |
+                r4(std::next(p, len - 4 - ((len >> 3U) << 2U)));
         }
         else if (len > 0)
         {
@@ -102,32 +106,32 @@ constexpr inline void mum(std::uint64_t* a, std::uint64_t* b)
     }
     else
     {
-        size_t i = len;
+        std::int64_t i = len;
         if (i > 48)
         {
             std::uint64_t see1 = seed;
             std::uint64_t see2 = seed;
             do
             {
-                seed = mix(r8(p) ^ secret[1], r8(p + 8) ^ seed);
-                see1 = mix(r8(p + 16) ^ secret[2], r8(p + 24) ^ see1);
-                see2 = mix(r8(p + 32) ^ secret[3], r8(p + 40) ^ see2);
-                p += 48;
+                seed = mix(r8(p) ^ secret[1], r8(std::next(p, 8)) ^ seed);
+                see1 = mix(r8(std::next(p, 16)) ^ secret[2], r8(std::next(p, 24)) ^ see1);
+                see2 = mix(r8(std::next(p, 32)) ^ secret[3], r8(std::next(p, 40)) ^ see2);
+                std::advance(p, 48);
                 i -= 48;
             } while (i > 48);
             seed ^= see1 ^ see2;
         }
         while (i > 16)
         {
-            seed = mix(r8(p) ^ secret[1], r8(p + 8) ^ seed);
+            seed = mix(r8(p) ^ secret[1], r8(std::next(p, 8)) ^ seed);
             i -= 16;
-            p += 16;
+            std::advance(p, 16);
         }
-        a = r8(p + i - 16);
-        b = r8(p + i - 8);
+        a = r8(std::next(p, i - 16));
+        b = r8(std::next(p, i - 8));
     }
 
-    return mix(secret[1] ^ len, mix(a ^ secret[1], b ^ seed));
+    return mix(secret[1] ^ static_cast<std::uint64_t>(len), mix(a ^ secret[1], b ^ seed));
 }
 
 [[nodiscard]] constexpr inline std::uint64_t hash(std::uint64_t x)
@@ -155,25 +159,26 @@ struct hash
 template <typename CharT>
 struct hash<std::basic_string<CharT>>
 {
-    constexpr std::uint64_t operator()(std::basic_string<CharT> const& str) const noexcept
+    std::uint64_t operator()(std::basic_string<CharT> const& str) const noexcept
     {
-        return wyhash_detail::hash(str.data(), sizeof(CharT) * str.size());
+        return wyhash_detail::hash(str.data(),
+                                   static_cast<std::int64_t>(sizeof(CharT) * str.size()));
     }
 };
 
 template <typename CharT>
 struct hash<std::basic_string_view<CharT>>
 {
-    constexpr std::uint64_t operator()(std::basic_string_view<CharT> const& sv) const noexcept
+    std::uint64_t operator()(std::basic_string_view<CharT> const& sv) const noexcept
     {
-        return wyhash_detail::hash(sv.data(), sizeof(CharT) * sv.size());
+        return wyhash_detail::hash(sv.data(), static_cast<std::int64_t>(sizeof(CharT) * sv.size()));
     }
 };
 
 template <class T>
 struct hash<T*>
 {
-    constexpr std::uint64_t operator()(T* ptr) const noexcept
+    std::uint64_t operator()(T* ptr) const noexcept
     {
         return wyhash_detail::hash(reinterpret_cast<uintptr_t>(ptr));
     }
