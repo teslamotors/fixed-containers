60d17e285af9a19d2afc37be1a875b3844eb6de3
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2024-04-17 17:21:19 -0400

    [FixedVector/FixedDeque] Use uninitialized_relocate in `erase`
    We can only use it when `!is_constant_evaluated`, since otherwise Clang
    complains about objects being accessed outside their lifetimes.

--- include/fixed_containers/algorithm.hpp
+++ include/fixed_containers/algorithm.hpp
@@ -4,9 +4,23 @@
 
 namespace fixed_containers::algorithm
 {
-// Similar to https://en.cppreference.com/w/cpp/algorithm/move_backward
-// but uses relocation instead of assignment
-// There seems to be an issue with clang and setting the active union member in a constexpr context.
+// Similar to https://en.cppreference.com/w/cpp/memory/uninitialized_move
+// but also destroys the source range
+template <class FwdIt1, class FwdIt2>
+constexpr FwdIt2 uninitialized_relocate(FwdIt1 first, FwdIt1 last, FwdIt2 d_first)
+{
+    while (first != last)
+    {
+        std::construct_at(&*d_first, std::move(*first));
+        std::destroy_at(&*first);
+        ++d_first;
+        ++first;
+    }
+    return d_first;
+}
+
+// Similar to https://en.cppreference.com/w/cpp/memory/uninitialized_move
+// but also destroys the source range
 template <class BidirIt1, class BidirIt2>
 constexpr BidirIt2 uninitialized_relocate_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last)
 {
