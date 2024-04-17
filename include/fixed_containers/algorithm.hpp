35038a55cba05827c2d1ae99a6ffc35cb94bbe47
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2024-04-17 17:11:24 -0400

    [FixedVector/FixedDeque] Rename `emplace_move_backward` to `uninitialized_relocate_backward`

--- include/fixed_containers/algorithm.hpp
+++ include/fixed_containers/algorithm.hpp
@@ -5,10 +5,10 @@
 namespace fixed_containers::algorithm
 {
 // Similar to https://en.cppreference.com/w/cpp/algorithm/move_backward
-// but uses `std::construct_at()`
+// but uses relocation instead of assignment
 // There seems to be an issue with clang and setting the active union member in a constexpr context.
 template <class BidirIt1, class BidirIt2>
-constexpr BidirIt2 emplace_move_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last)
+constexpr BidirIt2 uninitialized_relocate_backward(BidirIt1 first, BidirIt1 last, BidirIt2 d_last)
 {
     while (first != last)
     {
