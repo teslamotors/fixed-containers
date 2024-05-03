d7458ca24e48db5e086fd5884ebc68f07ad41439
Grey Golla <ggolla@tesla.com>
2024-05-02 19:22:49 -0700

    Introduce FixedDoublyLinkedListRawView
    Create `fixed_doubly_linked_list_raw_view`, which allows non-type safe
    inspection of a raw byte array, assuming it contains a
    `FixedDoublyLinkedList` of the specified maximum size and containing
    elements of the specified size. Only allows read-only forward iteration
    of the contained items.

---
+++ include/fixed_containers/fixed_doubly_linked_list_raw_view.hpp
@@ -0,0 +1,116 @@
+#pragma once
+
+#include "fixed_containers/fixed_doubly_linked_list.hpp"
+#include "fixed_containers/forward_iterator.hpp"
+
+#include <cstddef>
+
+namespace fixed_containers::fixed_doubly_linked_list_detail
+{
+// non-templated iterator over `FixedDoublyLinkedList`s allows inspection of the data type without
+// knowing the actual type of the underlying object
+template <typename IndexType = std::size_t>
+class FixedDoublyLinkedListRawView
+{
+    using ChainEntryType = LinkedListIndices<IndexType>;
+
+public:
+    class ReferenceProvider
+    {
+        friend class FixedDoublyLinkedListRawView;
+
+    private:
+        const FixedDoublyLinkedListRawView* parent_;
+
+        IndexType current_idx_;
+
+        explicit constexpr ReferenceProvider(const FixedDoublyLinkedListRawView* parent) noexcept
+          : parent_{parent}
+          , current_idx_{parent_->max_elem_count_}  // the start/end sentinel is at this index
+        {
+        }
+
+    public:
+        constexpr ReferenceProvider() noexcept
+          : ReferenceProvider(nullptr, {}, {})
+        {
+        }
+
+        constexpr void advance() noexcept { current_idx_ = parent_->indices_of(current_idx_).next; }
+
+        constexpr const std::byte* get() const noexcept { return parent_->value_at(current_idx_); }
+
+        constexpr bool operator==(const ReferenceProvider& other) const noexcept = default;
+    };
+
+private:
+    const std::byte* list_ptr_;
+    std::size_t elem_size_bytes_;
+    std::size_t max_elem_count_;
+
+public:
+    using Iterator =
+        ForwardIterator<ReferenceProvider, ReferenceProvider, IteratorConstness::CONSTANT_ITERATOR>;
+    using iterator = Iterator;
+    using const_iterator = iterator;
+
+    FixedDoublyLinkedListRawView(const void* list_ptr,
+                                 std::size_t elem_size_bytes,
+                                 std::size_t max_elem_count)
+      : list_ptr_{static_cast<const std::byte*>(list_ptr)}
+      // the PoolStorage stores unions of `T`, `std::size_t`, so they are always at least that big
+      , elem_size_bytes_{std::max(elem_size_bytes, sizeof(std::size_t))}
+      , max_elem_count_{max_elem_count}
+    {
+    }
+
+    Iterator begin() const { return std::next(Iterator{ReferenceProvider{this}}); }
+
+    Iterator end() const { return Iterator{ReferenceProvider{this}}; }
+
+    std::size_t size() const
+    {
+        // this is _very_ _very_ brittle and reliant on the size of every field in the
+        // `FixedDoublyLinkedList`!
+        return *reinterpret_cast<const std::size_t*>(
+            std::next(list_ptr_, value_storage_size() + chain_size()));
+    }
+
+public:
+    [[nodiscard]] constexpr std::ptrdiff_t value_storage_size() const noexcept
+    {
+        return static_cast<std::ptrdiff_t>(max_elem_count_ * elem_size_bytes_ +
+                                           sizeof(std::size_t));
+    }
+
+    [[nodiscard]] constexpr std::ptrdiff_t chain_size() const noexcept
+    {
+        return static_cast<std::ptrdiff_t>(sizeof(ChainEntryType) * (max_elem_count_ + 1));
+    }
+
+    [[nodiscard]] constexpr const std::byte* value_storage_start() const noexcept
+    {
+        // this is brittle and reliant on the layout of `FixedDoublyLinkedList`, yikes!
+        return list_ptr_;
+    }
+
+    [[nodiscard]] constexpr const std::byte* value_at(IndexType i) const noexcept
+    {
+        // this relies on `FixedIndexBasedPoolStorage` starting with its dense array of `Value`
+        return std::next(value_storage_start(), static_cast<std::ptrdiff_t>(elem_size_bytes_ * i));
+    }
+
+    [[nodiscard]] constexpr const ChainEntryType* chain_start() const noexcept
+    {
+        // this is _very_ brittle and reliant on the layout of `FixedDoublyLinkedList` _and_ the
+        // layout of `FixedIndexBasedPoolStorage` the storage holds the array + 1 `std::size_t` for
+        // the next index
+        return reinterpret_cast<const ChainEntryType*>(std::next(list_ptr_, value_storage_size()));
+    }
+
+    [[nodiscard]] constexpr const ChainEntryType& indices_of(IndexType i) const noexcept
+    {
+        return *std::next(chain_start(), static_cast<std::ptrdiff_t>(i));
+    }
+};
+}  // namespace fixed_containers::fixed_doubly_linked_list_detail
---
+++ test/fixed_doubly_linked_list_raw_view_test.cpp
@@ -0,0 +1,139 @@
+#include "fixed_containers/fixed_doubly_linked_list_raw_view.hpp"
+
+#include "test_utilities_common.hpp"
+
+#include "fixed_containers/fixed_doubly_linked_list.hpp"
+#include "fixed_containers/fixed_list.hpp"
+
+namespace fixed_containers
+{
+namespace
+{
+template <typename T>
+T get_from_ptr(const std::byte* ptr)
+{
+    return *reinterpret_cast<const T*>(ptr);
+}
+}  // namespace
+
+using namespace fixed_doubly_linked_list_detail;
+
+static_assert(std::forward_iterator<FixedDoublyLinkedListRawView<std::size_t>::iterator>);
+static_assert(std::ranges::forward_range<FixedDoublyLinkedListRawView<std::size_t>>);
+
+TEST(FixedDoublyLinkedListRawView, ViewOfIntegerList)
+{
+    FixedDoublyLinkedList<int, 10> list;
+
+    std::size_t ten = list.emplace_back_and_return_index(10);
+    list.emplace_back_and_return_index(20);
+    list.emplace_back_and_return_index(30);
+
+    auto view = FixedDoublyLinkedListRawView(&list, sizeof(int), 10);
+
+    EXPECT_EQ(offsetof(decltype(list), IMPLEMENTATION_DETAIL_DO_NOT_USE_storage_), 0);
+    EXPECT_EQ(offsetof(decltype(list), IMPLEMENTATION_DETAIL_DO_NOT_USE_chain_),
+              view.value_storage_size());
+    EXPECT_EQ(offsetof(decltype(list), IMPLEMENTATION_DETAIL_DO_NOT_USE_size_),
+              view.value_storage_size() + view.chain_size());
+
+    EXPECT_EQ(view.size(), 3);
+
+    auto it = view.begin();
+    EXPECT_EQ(get_from_ptr<int>(*it), 10);
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<int>(*it), 20);
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<int>(*it), 30);
+    it = std::next(it);
+    EXPECT_EQ(it, view.end());
+
+    list.emplace_front_and_return_index(-10);
+    list.emplace_front_and_return_index(-20);
+    list.emplace_back_and_return_index(40);
+    list.delete_at_and_return_next_index(ten);
+
+    // list is now -20, -10, 20, 30, 40 but with physical storage mixed around a bit
+    EXPECT_EQ(view.size(), 5);
+
+    it = view.begin();
+    EXPECT_EQ(get_from_ptr<int>(*it), -20);
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<int>(*it), -10);
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<int>(*it), 20);
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<int>(*it), 30);
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<int>(*it), 40);
+    it = std::next(it);
+    EXPECT_EQ(it, view.end());
+}
+
+struct StructThatContainsPadding
+{
+    char a;
+    size_t b;
+
+    constexpr bool operator==(const StructThatContainsPadding& other) const = default;
+};
+
+TEST(FixedDoublyLinkedListRawView, ViewOfStructList)
+{
+    FixedDoublyLinkedList<StructThatContainsPadding, 5> list;
+
+    std::size_t first = list.emplace_back_and_return_index(StructThatContainsPadding{'a', 123});
+    list.emplace_back_and_return_index(StructThatContainsPadding{'b', 234});
+    list.emplace_back_and_return_index(StructThatContainsPadding{'c', 345});
+    list.emplace_front_and_return_index(StructThatContainsPadding{'Z', 321});
+    list.delete_at_and_return_next_index(first);
+    list.emplace_front_and_return_index(StructThatContainsPadding{'Y', 432});
+    list.emplace_back_and_return_index(StructThatContainsPadding{'d', 456});
+    // list is Y, Z, b, c, d
+
+    auto view = FixedDoublyLinkedListRawView(
+        reinterpret_cast<void*>(&list), sizeof(StructThatContainsPadding), 5);
+
+    EXPECT_EQ(offsetof(decltype(list), IMPLEMENTATION_DETAIL_DO_NOT_USE_storage_), 0);
+    EXPECT_EQ(offsetof(decltype(list), IMPLEMENTATION_DETAIL_DO_NOT_USE_chain_),
+              view.value_storage_size());
+    EXPECT_EQ(offsetof(decltype(list), IMPLEMENTATION_DETAIL_DO_NOT_USE_size_),
+              view.value_storage_size() + view.chain_size());
+
+    EXPECT_EQ(view.size(), 5);
+
+    auto it = view.begin();
+    EXPECT_EQ(get_from_ptr<StructThatContainsPadding>(*it), (StructThatContainsPadding{'Y', 432}));
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<StructThatContainsPadding>(*it), (StructThatContainsPadding{'Z', 321}));
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<StructThatContainsPadding>(*it), (StructThatContainsPadding{'b', 234}));
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<StructThatContainsPadding>(*it), (StructThatContainsPadding{'c', 345}));
+    it = std::next(it);
+    EXPECT_EQ(get_from_ptr<StructThatContainsPadding>(*it), (StructThatContainsPadding{'d', 456}));
+    it = std::next(it);
+    EXPECT_EQ(it, view.end());
+}
+
+TEST(FixedDoublyLinkedListRawView, ViewOfFixedList)
+{
+    auto list = make_fixed_list({1.0, 2.9, 3.8, 4.7});
+
+    auto view = FixedDoublyLinkedListRawView(reinterpret_cast<void*>(&list), sizeof(double), 4);
+
+    EXPECT_EQ(view.size(), 4);
+
+    auto it = view.begin();
+    EXPECT_DOUBLE_EQ(get_from_ptr<double>(*it), 1.0);
+    it = std::next(it);
+    EXPECT_DOUBLE_EQ(get_from_ptr<double>(*it), 2.9);
+    it = std::next(it);
+    EXPECT_DOUBLE_EQ(get_from_ptr<double>(*it), 3.8);
+    it = std::next(it);
+    EXPECT_DOUBLE_EQ(get_from_ptr<double>(*it), 4.7);
+    it = std::next(it);
+    EXPECT_EQ(it, view.end());
+}
+
+}  // namespace fixed_containers
