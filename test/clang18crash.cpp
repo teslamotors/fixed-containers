d6a47e33dd7f930048ee1082cc6165e02023dddd
Alexander Karatarakis <alex@karatarakis.com>
2024-05-18 23:13:54 -0700

    clang-18 crash minmin-repro

--- test/clang18crash.cpp
+++ test/clang18crash.cpp
@@ -1,18 +1,25 @@
-#include <fixed_containers/fixed_vector.hpp>
+#include <memory>
+#include <source_location>
 
-struct MockFixedString
+struct IntConstuctible
 {
-    explicit(false) constexpr MockFixedString(
-        const std::string_view& /*view*/,
-        const std::source_location& /*loc*/ = std::source_location::current()) noexcept
+    IntConstuctible() = default;
+
+    IntConstuctible(int,
+                    const std::source_location& /*loc*/ = std::source_location::current()) noexcept
     {
     }
 };
 
+static auto construct_at_address_of(IntConstuctible& p, int a)
+{
+    //
+    return std::construct_at(&p, a);
+}
+
 int main()
 {
-    fixed_containers::FixedVector<MockFixedString, 12> nodes{};
-    std::string_view name_view{};
-    nodes.emplace_back(name_view);
+    IntConstuctible instance{};
+    construct_at_address_of(instance, 1);
     return 0;
 }
