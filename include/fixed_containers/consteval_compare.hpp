b490837b6720807055614c2895effd4343cb1e68
Alexander Karatarakis <alex@karatarakis.com>
2024-06-08 18:30:21 -0700

    [consteval_compare::equal] Suppress erroneous diagnostic

--- include/fixed_containers/consteval_compare.hpp
+++ include/fixed_containers/consteval_compare.hpp
@@ -4,17 +4,17 @@ namespace fixed_containers::consteval_compare
 {
 // These will show the values if it fails to compile
 template <auto LHS, auto RHS>
-inline constexpr bool equal = LHS == RHS;
+inline constexpr bool equal = (LHS == RHS);  // NOLINT(google-readability-casting)
 template <auto LHS, auto RHS>
-inline constexpr bool not_equal = LHS != RHS;
+inline constexpr bool not_equal = (LHS != RHS);
 
 template <auto LHS, auto RHS>
-inline constexpr bool less = LHS < RHS;
+inline constexpr bool less = (LHS < RHS);
 template <auto LHS, auto RHS>
-inline constexpr bool less_or_equal = LHS <= RHS;
+inline constexpr bool less_or_equal = (LHS <= RHS);
 
 template <auto LHS, auto RHS>
-inline constexpr bool greater = LHS > RHS;
+inline constexpr bool greater = (LHS > RHS);
 template <auto LHS, auto RHS>
-inline constexpr bool greater_or_equal = LHS >= RHS;
+inline constexpr bool greater_or_equal = (LHS >= RHS);
 }  // namespace fixed_containers::consteval_compare
