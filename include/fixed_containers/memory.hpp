bba857c14e6380407c83c6f01d366cef57c4282f
Alexander Karatarakis <alex@karatarakis.com>
2024-06-17 15:46:14 -0700

    [memory] clang-18 workaround

--- include/fixed_containers/memory.hpp
+++ include/fixed_containers/memory.hpp
@@ -6,10 +6,16 @@ namespace fixed_containers::memory
 {
 // Similar to https://en.cppreference.com/w/cpp/memory/construct_at
 // but uses references and correctly handles types that overload operator&
+//
+// Returning `auto` has issues with clang-18 - causes a compiler crash.
+// One bug is: https://github.com/llvm/llvm-project/issues/92680
+// There appears to be more, to be investigated.
+// Returning an explicit `T*` also fails in certain cases (msvc).
+// As a workaround, don't return anything, which is a minor divergence with `std::construct_at()`.
 template <typename T, typename ... Args>
-constexpr auto construct_at_address_of(T& p, Args&&... args)
+constexpr void construct_at_address_of(T& p, Args&&... args)
 {
-    return std::construct_at(std::addressof(p), std::forward<Args>(args)...);
+    std::construct_at(std::addressof(p), std::forward<Args>(args)...);
 }
 
 // Similar to https://en.cppreference.com/w/cpp/memory/destroy_at
--- test/memory_test.cpp
+++ test/memory_test.cpp
@@ -9,9 +9,11 @@ TEST(ConstructAtAddressOf, CArray)
 #if !defined(_MSC_VER)
     int a[5]{};
     memory::destroy_at_address_of(a);
+#if 0
     int* result = memory::construct_at_address_of(a);
     result[0] = 99;
     ASSERT_TRUE(result == static_cast<int*>(a));
+#endif
 #endif
 }
 }  // namespace fixed_containers
