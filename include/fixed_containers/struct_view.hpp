3ae45da77deba10850fb953094c6bfd15138eb5e
Alexander Karatarakis <alex@karatarakis.com>
2024-06-27 14:15:33 -0700

    Introduce struct_view skeleton

---
+++ include/fixed_containers/struct_view.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace fixed_containers::struct_view
+{
+
+}
---
+++ test/struct_view_test.cpp
@@ -0,0 +1,15 @@
+#if defined(__clang__) && __clang_major__ >= 15
+
+// #include "fixed_containers/struct_view.hpp"
+
+#include <gtest/gtest.h>
+
+namespace fixed_containers::struct_view
+{
+TEST(StructView, Placeholder)
+{
+    //
+}
+}  // namespace fixed_containers::struct_view
+
+#endif
