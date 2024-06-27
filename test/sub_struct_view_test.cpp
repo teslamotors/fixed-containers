c893bfb91e596d83cf897fed7ccfc92eb72651e7
Alexander Karatarakis <alex@karatarakis.com>
2024-06-27 14:15:33 -0700

    Introduce substruct_view skeleton

---
+++ test/sub_struct_view_test.cpp
@@ -0,0 +1,15 @@
+#if defined(__clang__) && __clang_major__ >= 15
+
+// #include "fixed_containers/sub_struct_view.hpp"
+
+#include <gtest/gtest.h>
+
+namespace fixed_containers::sub_struct_view
+{
+TEST(SubStructView, Placeholder)
+{
+    //
+}
+}  // namespace fixed_containers::sub_struct_view
+
+#endif
