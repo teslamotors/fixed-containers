5f5045f62c3de918a12d5867f53df23ef42888c5
Younes Reda <yreda@tesla.com>
2024-07-05 10:14:07 -0700

    introduce FixedString::operator+(FixedString)

--- test/fixed_string_test.cpp
+++ test/fixed_string_test.cpp
@@ -1582,6 +1582,11 @@ TEST(FixedStringTest, OStreamOperator)
     EXPECT_EQ(ss.str(), "hello");
 }
 
+TEST(FixedStringTest, PlusOperator)
+{
+    static_assert(make_fixed_string("hello world") == (make_fixed_string("hello ") + make_fixed_string("world")));
+}
+
 namespace
 {
 template <FixedString<5> /*MY_STR*/>
