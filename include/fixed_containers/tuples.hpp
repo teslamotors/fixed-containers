bac4457b29a43020e97b70f06fe8c3dbe0469e10
Alexander Karatarakis <alex@karatarakis.com>
2024-12-12 20:00:21 -0800

    Remove as_tuple_view()
    Mapping a struct to a tuple can be very slow for high enough sizes,
    which the practical reason it was limited to <=400 fields.
    
    The functionality can be serviced by the previously introduced
    struct_decomposition

--- include/fixed_containers/tuples.hpp
+++ include/fixed_containers/tuples.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "fixed_containers/tuples_as_tuple_view.hpp"
-
 #include <concepts>
 #include <cstddef>
 #include <tuple>
@@ -9,12 +7,6 @@
 
 namespace fixed_containers::tuples
 {
-template <std::size_t FIELD_COUNT, typename T>
-constexpr auto as_tuple_view(T& data)
-{
-    return as_tuple_view_detail::as_tuple_view<FIELD_COUNT, T>(data);
-}
-
 template <typename Tuple, typename Func>
     requires(std::tuple_size_v<std::decay_t<Tuple>> == 0)
 constexpr void for_each_entry(Tuple&& /*tuple*/, Func&& /*func*/)
