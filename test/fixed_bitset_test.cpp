607aa892cd48b7d0adbd4f2ec8e993d3d8025df7
Alexander Karatarakis <alex@karatarakis.com>
2025-04-22 14:01:41 -0700

    [FixedBitset] Add test for triviality and structural type

--- test/fixed_bitset_test.cpp
+++ test/fixed_bitset_test.cpp
@@ -1,9 +1,18 @@
 #include "fixed_containers/fixed_bitset.hpp"
 
+#include "fixed_containers/concepts.hpp"
+
 #include <gtest/gtest.h>
 
 namespace fixed_containers
 {
+
+using FixedBitsetType = FixedBitset<5>;
+static_assert(TriviallyCopyable<FixedBitsetType>);
+static_assert(NotTrivial<FixedBitsetType>);
+static_assert(StandardLayout<FixedBitsetType>);
+static_assert(IsStructuralType<FixedBitsetType>);
+
 TEST(FixedBitset, DefaultConstructor)
 {
     constexpr FixedBitset<8> VAL1{};
